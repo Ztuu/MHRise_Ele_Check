124e2bbfd7246716e815cc966de5bd81907987f7
Stuart <stu@wigwamholidays.com>
2021-02-01 17:54:35 +0000

    Add more to about this app modal

--- components/AboutModal.js
+++ components/AboutModal.js
@@ -1,19 +1,23 @@
 // External imports
 import React from 'react';
-import { StyleSheet, Text, View, Button, Modal } from 'react-native';
+import { StyleSheet, Text, View, Button, Modal, Dimensions } from 'react-native';
 import * as Linking from 'expo-linking';
 
 // Internal imports
 import CustomButton from './CustomButton'
+import CustomText from './CustomText'
 
+var screenWidth = Dimensions.get('window').width; //full width
 
 export default function ModalScreen({ navigation }) {
   return (
     <View style={styles.modalBody}>
-      <Text style={{ fontSize: 30, fontWeight: "bold" }}>About This App</Text>
-      <Text></Text>
-      <CustomButton title="View Source Code" onPress={() => Linking.openURL('https://github.com/Ztuu/MHRise_React')} />
-
+      <CustomText text="About This App" bold="true" style={{ fontSize: 30}} />
+      <CustomText text="This app was created by Stuart Paterson and is open source, released under the MIT license." style={{paddingBottom: 20}}/>
+      <CustomText text="The full source code can be viewed  on Github by clicking the link below. Feel free to copy it for your own project or contribute if you have any ideas!"/>
+      <CustomButton title="View Source Code" onPress={() => Linking.openURL('https://github.com/Ztuu/MHRise_React')} customClass={{marginBottom: 20}} />
+      <CustomText text="If you notice any issues or mistakes in the app please let me know." />
+      <CustomButton title="Email Developer" onPress={() => Linking.openURL('mailto:stuartpdev@gmail.com')} />
       <CustomButton customClass={{marginTop: 20, backgroundColor: "#ff6054"}} onPress={() => navigation.goBack()} title="Close" />
     </View>
   );
@@ -22,9 +26,10 @@ export default function ModalScreen({ navigation }) {
 const styles = StyleSheet.create({
   modalBody: {
      flex: 1,
-     alignItems: 'center',
+     alignItems: 'flex-start',
      justifyContent: 'center',
-     backgroundColor: "#919191"
+     backgroundColor: "#919191",
+     paddingHorizontal: screenWidth/15,
   },
 
 });
